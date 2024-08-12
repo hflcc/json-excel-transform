1de3b8198e7582c3ac2ac0c4e30f39b29a4c8954
Hing <34307008+hflcc@users.noreply.github.com>
2024-08-12 16:02:32 +0800

    feature: init

---
+++ src/transExcelToJson.js
@@ -0,0 +1,36 @@
+const xlsx = require("xlsx");
+const fs = require("fs");
+const path = require("path");
+
+const excelFoldersPath = path.join(__dirname, "../excel");
+const excelFiles = fs.readdirSync(excelFoldersPath, { encoding: "utf-8" });
+
+excelFiles.forEach((excelFile) => {
+  // 读取 Excel 文件
+  const workbook = xlsx.readFile(path.join(__dirname, '../excel/', excelFile));
+
+  // 选择第一个工作表
+  const sheetName = workbook.SheetNames[0];
+  const worksheet = workbook.Sheets[sheetName];
+
+  // 将 Excel 中的数据转换为 JSON 格式
+  const arrData = xlsx.utils.sheet_to_json(worksheet, { header: "A" });
+
+  // 将第一列作为对象的键，第三列作为对象的值
+  const dataObject = {};
+  arrData.forEach((item, index) => {
+    if (index === 0) return // 第一行是表头，不需要
+    const { A: key, C: value } = item;
+    if (key !== undefined && value !== undefined) {
+      dataObject[key] = value;
+    }
+  });
+
+  // 将对象写入到文件中
+  fs.writeFile(path.join(__dirname, "../dist", excelFile.split('.')[0] + '.json'), JSON.stringify(dataObject, null, 2), (err) => {
+    if (err) {
+      console.error("Error writing file:", err);
+      return;
+    }
+  });
+});
---
+++ src/transJsonToExcel.js
@@ -0,0 +1,41 @@
+const XLSX = require('xlsx');
+const fs = require('fs');
+const path = require('path')
+
+const jsonFoldersPath = path.join(__dirname, '../json/')
+const jsonFiles = fs.readdirSync(jsonFoldersPath, {encoding: 'utf-8'})
+
+jsonFiles.forEach(jsonFileName => {
+    const filePath = path.join(__dirname, '../json/' + jsonFileName)
+    const jsonData = require(filePath)
+
+    // 创建一个新的工作簿
+    const workbook = XLSX.utils.book_new();
+
+    // 表名
+    const sheetName = "Sheet1";
+
+
+
+    // 将 JSON 数据转换为工作表数据
+    // const data = [['语言key', '语言值', '语言类型', '语言包类型', '所在模块', '来源字段']];
+    const data = [['语言key', '英语值']];
+
+    for (const key in jsonData) {
+      data.push([key, jsonData[key]]);
+    }
+
+    // 创建工作表
+    const worksheet = XLSX.utils.aoa_to_sheet(data);
+
+
+    // 将工作表添加到工作簿
+    XLSX.utils.book_append_sheet(workbook, worksheet, sheetName);
+
+
+
+    // 将工作簿写入 Excel 文件
+    const excelFileName = path.join(__dirname, `../dist/${jsonFileName.split('.')[0]}.xlsx`);
+
+    XLSX.writeFile(workbook, excelFileName, { bookType: 'xlsx', type: 'file' });
+})
